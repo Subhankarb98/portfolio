7b829d576db2733a2a4eed54ef10dcbe7e163807
Subhankarb98 <112893286+Subhankarb98@users.noreply.github.com>
2024-09-06 15:38:56 +0530

    Update home.js

--- home.js
+++ home.js
@@ -1,5 +1,18 @@
-// $("#interests .card").mouseover(function() {
-//     $(this).css("box-shadow", "0 10px 15px -3px rgba(0, 0, 0, 0.1), 0 4px 6px -4px rgba(0, 0, 0, 0.1)");
-//  }).mouseleave(function(){
-//    $(this).css("box-shadow", "0px 0px 0px #555");
-// });
+$(".nav-item").mouseover(function() {
+    $(this).css("box-shadow", "0 10px 15px -3px rgba(0, 0, 0, 0.1), 0 4px 6px -4px rgba(0, 0, 0, 0.1)");
+ }).mouseleave(function(){
+   $(this).css("box-shadow", "0px 0px 0px #555");
+});
+
+$(".intro h1, .intro h5").mouseover(function() {
+    $(this).css("color","#F5F5F5");
+    }).mouseleave(function() {
+        $(this).css("color","black");
+});
+
+
+$("#profile-pic").mouseover(function() {
+    $(this).css("border-color","#F5F5F5");
+    }).mouseleave(function() {
+        $(this).css("border-color","black");
+});
