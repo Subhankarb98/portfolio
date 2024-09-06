ddd41ab44606cc770ab26132425fa21ad5f763d8
Subhankarb98 <112893286+Subhankarb98@users.noreply.github.com>
2024-09-06 15:19:12 +0530

    Add files via upload

---
+++ home.js
@@ -0,0 +1,5 @@
+// $("#interests .card").mouseover(function() {
+//     $(this).css("box-shadow", "0 10px 15px -3px rgba(0, 0, 0, 0.1), 0 4px 6px -4px rgba(0, 0, 0, 0.1)");
+//  }).mouseleave(function(){
+//    $(this).css("box-shadow", "0px 0px 0px #555");
+// });
