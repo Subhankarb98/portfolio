987e4b3d3afee4fc50710005ad8fb90d4bb3f7b4
Subhankarb98 <112893286+Subhankarb98@users.noreply.github.com>
2024-09-11 10:55:02 +0530

    Update index.js

--- javascript/index.js
+++ javascript/index.js
@@ -3,3 +3,9 @@ $("h1").mouseover(function() {
     }).mouseleave(function() {
         $(this).css("text-shadow","none")
 });
+
+$(".btn").mouseover(function() {
+    $(this).css("box-shadow", "0 10px 50px -3px rgba(0, 0, 0, 0.1), 0 4px 6px -4px rgba(0, 0, 0, 0.1)");
+ }).mouseleave(function(){
+   $(this).css("box-shadow", "0px 0px 0px #555");
+});
